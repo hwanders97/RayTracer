c2e74d54026e9d223050ceef9356eb966639d98d
hwanders97 <hwandersman@gmail.com>
2017-01-15 21:17:37 -0500

    Update refraction

--- sphere.cpp
+++ sphere.cpp
@@ -13,18 +13,20 @@ using namespace std;
 // Default constructor
 Sphere::Sphere() : center(Point()), radius(1) {
   material = new Material();
-  shadow = false;
+  checkshadow = false;
+  castshadow = false;
 }
 
 // Input constructor
-Sphere::Sphere(float x, float y, float z, float rad, Material* mat, bool shade) : center(Point(x, y, z)), radius(rad) {
+Sphere::Sphere(float x, float y, float z, float rad, Material* mat, bool checkshade, bool castshade) : center(Point(x, y, z)), radius(rad) {
   // Inherited members must be manually initialized
   this->material = mat;
-  this->shadow = shade;
+  this->checkshadow = checkshade;
+  this->castshadow = castshade;
 }
 
 // Takes in a ray and gives back the point of intersection and normal if there is one
-bool Sphere::intersect(Ray& ray, Local* local, bool shadows) {
+void Sphere::intersect(Ray& ray, Local* local, bool shadows, int* retval) {
   // Necessary variables
   float discriminant, a, b, c, t1, t2;
   // Ray from camera to center of sphere
@@ -36,44 +38,42 @@ bool Sphere::intersect(Ray& ray, Local* local, bool shadows) {
   c = Vector::dot(temp, temp) - (radius * radius);
   discriminant = (b * b) - (4 * a *c);
   
+  *retval = 0;
+  
   // If discriminant is negative then there are no real roots and no intersection
-  if (discriminant < 0)
-    return false;
-  else {
+  if (discriminant >= 0) {
+    // No need to store intersection information if point is in shadow
     if (shadows)
-      return true;
-    
-    t1 = (-b + sqrt(discriminant)) / (2 * a);
-    t2 = (-b - sqrt(discriminant)) / (2 * a);
+      *retval = 1;
+    else  {
+      t1 = (-b + sqrt(discriminant)) / (2 * a);
+      t2 = (-b - sqrt(discriminant)) / (2 * a);
 
-    // 2 real positive roots, pick smaller if in range t_min : t_max
-    if (t1 > 0 && t2 > 0) {
-      if (t1 < t2 && t1 > ray.t_min && t1 < ray.t_max)
-        ray.t = t1;
-      else if (t2 < t1 && t2 > ray.t_min && t2 < ray.t_max)
-        ray.t = t2;
-      else return false;
+      // t1 is always larger than t2
+      if (t1 > 0) {
+        // 2 real positive roots, pick smaller if in range t_min : t_max
+        // Outside sphere
+        if (t2 > 0 && t2 < ray.t_max) {
+          ray.t = t2;
+          *retval = 1;
+        }
+        // One positive, one negative, choose positive
+        // Inside sphere
+        else if (t1 < ray.t_max){
+          ray.t = t1;
+          *retval = -1;
+        }
+        
+        // If a t value is stored
+        if (*retval) {
+          // Store local information
+          local->pos = ray.pos + (ray.dir * ray.t);
+          temp = local->pos - center;
+          local->normal = Vector::normalize(temp);
+          local->material = material;
+        }
+      }
     }
-    // Both roots same, tangent to sphere
-    else if (t1 == t2) {
-      if (t1 > ray.t_min && t1 < ray.t_max)
-        ray.t = t1;
-      else return false;
-    }
-    // One positive, one negative, choose positive
-    else {
-      if (t1 > 0 && t1 > ray.t_min && t1 < ray.t_max)
-        ray.t = t1;
-      else if (t2 > 0 && t2 > ray.t_min && t2 < ray.t_max)
-        ray.t = t2;
-      else return false;
-    }
-    // Store local information
-    local->pos = ray.pos + (ray.dir * ray.t);
-    temp = local->pos - center;
-    local->normal = Vector::normalize(temp);
-    local->material = material;
-    return true;
   }
 }
 
